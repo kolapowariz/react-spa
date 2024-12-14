4188269a0c78b079c865b930a8bdc3f94e114961
Kolapo Wariz <kolapowariz@gmail.com>
2024-12-14 20:23:59 +0100

    React SPA

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Post from "./components/Post"
+
+function App() {
+  return (
+    <main>
+      <Post body="React course" author="Kolapo"/>
+      <Post body="React is good" author="Wariz"/>
+    </main>
+  )
+}
+
+export default App
